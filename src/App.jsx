8fc4ed39a37f9a454d0afe65cbb516f55c767b51
TomislavPe <130553449+TomislavPe@users.noreply.github.com>
2023-09-20 18:38:42 +0200

    lista primjer

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,13 @@
-import ComponentClass from "./ComponentClass";
-import ComponentFunction from "./ComponentFunction";
 import "./App.css";
-import { useState } from "react";
 
 function App() {
-    const [isCompVisible, setIsCompVisible] = useState(true);
-
-    const handleToggleComponent = () => {
-        setIsCompVisible((prev) => !prev);
-    };
+    const niz = ["jedan", "dva", "tri"];
 
     return (
         <>
-            {isCompVisible ? <ComponentClass /> : <ComponentFunction />}
-            <button onClick={handleToggleComponent}>Toggle Component</button>
+            {niz.map((stringBroj, index) => (
+                <li key={index}>{stringBroj}</li>
+            ))}
         </>
     );
 }
---
+++ src/user/UserFunction.jsx
@@ -0,0 +1,10 @@
+export default function UserClass({ name, years, changeName }) {
+    return (
+        <>
+            <p>
+                Pozdrav, moje ime je {name} i imam {years} godina
+            </p>
+            <input type="text" value={name} onChange={changeName} />
+        </>
+    );
+}
